7164a4754a735b4ee7e28f2905e4de1ae6cb0581
juliasut <juliasutygina@gmail.com>
2021-08-13 17:25:33 -0600

    Add lyfecycle events

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -5,9 +5,9 @@ class Panel extends Component {
     const { label, value, onSelect } = this.props;
 
     return (
-      <section classnName="dashboard__panel" onClick={onSelect} >
-        <h1 classnName="dashboard__panel-header">{label}</h1>
-        <p classnName="dashboard__panel-value">{value}</p>
+      <section className="dashboard__panel" onClick={onSelect} >
+        <h1 className="dashboard__panel-header">{label}</h1>
+        <p className="dashboard__panel-value">{value}</p>
       </section>
     );
   }
